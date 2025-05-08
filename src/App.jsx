43e846b836f5ced870fdcff214afeafd1f577642
Vinayak Singh <ishita.rs27@gmail.com>
2025-05-08 19:30:38 +0530

    updated

--- src/App.jsx
+++ src/App.jsx
@@ -7,8 +7,24 @@ import SocialLinks from './components/SocialLinks';
 import Contact from './components/Contact';
 import Footer from './components/Footer';
 import Navbar from './components/navbar';
-
+import Lenis from '@studio-freight/lenis';
+import { useEffect } from 'react';
 function App() {
+  useEffect(() => {
+    const lenis = new Lenis({
+      smooth: true,
+      lerp: 0.1, // default is 0.1; lower = smoother
+    })
+
+    function raf(time) {
+      lenis.raf(time)
+      requestAnimationFrame(raf)
+    }
+
+    requestAnimationFrame(raf)
+
+    return () => lenis.destroy()
+  }, [])
   return (
     <BrowserRouter>
       <div className="font-sans bg-black min-h-screen flex flex-col">
